3d0086af81893923d0318376361444e96d8d27b9
Eibon666 <72273688+Eibon666@users.noreply.github.com>
2021-01-13 12:59:22 -0600

    Add files via upload

---
+++ Project2/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Project2
+//
+//  Created by P.M. Student on 1/13/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("We are the Death Korps of Krieg")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
